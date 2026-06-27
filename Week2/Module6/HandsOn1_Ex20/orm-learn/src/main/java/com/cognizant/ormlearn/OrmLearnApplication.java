de50275f6e3335bbf0d40543d5b6492d82997d7d
santhiyasss <santhiyasaravanakumar23@gmail.com>
2026-06-27 18:42:26 +0530

    Add Week2 Module6 Spring Data JPA - HandsOn1 19&20 Country operations

---
+++ Week2/Module6/HandsOn1_Ex20/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,39 @@
+package com.cognizant.ormlearn;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryNotFoundException;
+import com.cognizant.ormlearn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER =
+        LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    private static CountryService countryService;
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+            SpringApplication.run(OrmLearnApplication.class, args);
+        LOGGER.info("Inside main");
+
+        countryService = context.getBean(CountryService.class);
+        findCountryByCodeTest();
+    }
+
+    private static void findCountryByCodeTest() {
+        LOGGER.info("Start");
+        try {
+            Country country = countryService.findCountryByCode("IN");
+            LOGGER.debug("Country: {}", country);
+            LOGGER.info("End");
+        } catch (CountryNotFoundException e) {
+            LOGGER.error("Country not found: {}", e.getMessage());
+        }
+    }
+}
