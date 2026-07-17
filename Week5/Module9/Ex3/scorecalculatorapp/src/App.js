94567d2aa94eae458c8fe6123f9c6c08a2dea77d
santhiyasss <santhiyasaravanakumar23@gmail.com>
2026-07-17 08:05:51 +0530

    Completed React Ex3 - ScoreCalculatorApp with CalculateScore function component

---
+++ Week5/Module9/Ex3/scorecalculatorapp/src/App.js
@@ -0,0 +1,16 @@
+import { CalculateScore } from './Components/CalculateScore';
+
+function App() {
+    return (
+        <div>
+            <CalculateScore
+                Name={"Santhiya"}
+                School={"Srivi Lions Matric Hr.Sec.School"}
+                total={284}
+                goal={3}
+            />
+        </div>
+    )
+}
+
+export default App;
