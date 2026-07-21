d6de51804c8786ab525a296c9225dc2c51ea31e0
santhiyasss <santhiyasaravanakumar23@gmail.com>
2026-07-21 12:05:12 +0530

    React Ex12 completed

---
+++ Week5/Module9/Ex12/ticketbookingapp/src/GuestGreeting.js
@@ -0,0 +1,15 @@
+function GuestGreeting(props) {
+    return (
+        <div>
+            <h2>Please sign up.</h2>
+            <h3>Available Flights</h3>
+            <ul>
+                <li>Flight AI101 - Chennai to Delhi - 10:00 AM</li>
+                <li>Flight AI202 - Chennai to Mumbai - 1:00 PM</li>
+                <li>Flight AI303 - Chennai to Bangalore - 4:00 PM</li>
+            </ul>
+        </div>
+    );
+}
+
+export default GuestGreeting;
---
+++ Week5/Module9/Ex12/ticketbookingapp/src/UserGreeting.js
@@ -0,0 +1,11 @@
+function UserGreeting(props) {
+    return (
+        <div>
+            <h2>Welcome back!</h2>
+            <h3>You can now book your tickets.</h3>
+            <button>Book Ticket</button>
+        </div>
+    );
+}
+
+export default UserGreeting;
